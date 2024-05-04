31c3b8db896c08bbde4634bb10ab3ec4a66f4c86
MahmadHadi <nayanihadi97@gmail.com>
2024-05-04 18:33:41 +0530

    4 may 24

---
+++ writeMethod.js
@@ -0,0 +1,4 @@
+let fName = 'hadi '
+document.write(`<h1><i><u>Hello</u></i></h1> <br> ${fName} world `)
+document.write('<h1>hello</h1> <br> &nbsp; ' + 'world ')
+document.write("<h3  >hello</h3> <br> &nbsp; " + "world" )
